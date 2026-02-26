7bd4906376f353d41302fbad562057e74e5de502
labcat_ <boldyrev0205@gmail.com>
2026-02-26 13:22:30 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+double function(double x) {
+return x*x-10/sqrt(100-x);}
+
+double functionDerivative(double x) {
+    return 2*x+5/((100-x)*sqrt(100-x));
+}
+double approximation(double x1, double limit) {
+    double x2 = x1-function(x1)/functionDerivative(x1);
+    while (fabs(x2-x1) > limit) {
+        x2 = x1;
+        x1 = (x2-function(x2)/functionDerivative(x2));
+    }
+    return x1;
+}
+int main(void) {
+    double x1;
+    printf("Input first approximation:");
+    scanf("%lf", &x1);
+    double limit;
+    printf("Input maximum deviation:");
+    scanf("%lf", &limit);
+    printf("Approximate zero point: %f", approximation(x1, limit));
+}
